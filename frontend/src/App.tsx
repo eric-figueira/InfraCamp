a2e0b093987fe36a3517c61b02add5312826e545
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2023-04-06 16:27:50 -0300

    Working on Recuperacao de Senha

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import './App.css';
 
-import Button from './components/layout/Button/Button';
-import TextInput from './components/layout/Input/TextInput';
-
-import { House } from 'phosphor-react'
+import RecuperacaoSenha from './components/pages/RecuperacaoSenha';
 
 function App() {
 
-  function Test() {
-    alert('aa')
-  }
-
   return (
     <div className="App">
-      <Button text='Entrar' backgroundColor='#000000' fontColor='#FFFFFF' eventHandler={Test} />
-      <Button text='Logar' backgroundColor='#FFF' fontColor='#000' borderColor='#000' eventHandler={Test} />
-      <TextInput placeholder='Senha' backgroundColor='#d4d4d4' fontColor='#000' fontSize={20} icon={House} iconSize={34} iconColor='#fff'/>
+      <RecuperacaoSenha />
     </div>
   );
 }
---
+++ frontend/src/components/pages/RecuperacaoSenha.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import "./RecuperacaoSenha.css"
+
+import { ReactComponent as ImagemRecuperacaoSenha } from "../../assets/imgs/imgRecSenha.svg"
+
+import Button from '../layout/Button/Button';
+import TextInput from '../layout/Input/TextInput';
+
+import { EnvelopeSimple, Key  } from 'phosphor-react'
+
+const RecuperacaoSenha: React.FC = () => {
+
+  function Test() {}
+
+  return (
+    <div className='wrapper'>
+      <div className="img-container">
+        <ImagemRecuperacaoSenha className='senha-img'/>
+      </div>
+      <div className="info-container">
+        <div className="form-wrapper">
+          <h1>Recupere sua senha</h1>
+          <form>
+            <TextInput placeholder='Email' backgroundColor='#F2F2F2' fontColor='#C1C2BD' fontSize={25} icon={EnvelopeSimple} iconSize={34} iconColor='#C1C2BD'/>
+            <TextInput placeholder='Nova Senha' backgroundColor='#F2F2F2' fontColor='#C1C2BD' fontSize={25} icon={Key} iconSize={34} iconColor='#C1C2BD'/>
+            <TextInput placeholder='Confirme a Senha' backgroundColor='#F2F2F2' fontColor='#C1C2BD' fontSize={25} icon={Key} iconSize={34} iconColor='#C1C2BD'/>
+            <Button text='Recuperar' backgroundColor='#1F2026' fontColor='#FFF' fontSize={25} eventHandler={Test} />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default RecuperacaoSenha;
