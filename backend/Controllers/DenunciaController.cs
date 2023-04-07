6c9c16450b9b2a43e435d3b265bf278e78f870d5
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2023-04-07 11:33:08 -0300

    Update DenunciaController.cs

--- backend/Controllers/DenunciaController.cs
+++ backend/Controllers/DenunciaController.cs
@@ -19,5 +19,26 @@ namespace backend.Controllers
         public DenunciaController(InfraCampContext ctx) {
             this._context = ctx;
         }
+
+        /*
+            Precisa de:
+                1 - GET de todas as denúncias
+                2 - GET de denúncias que o usuário fez
+                3 - GET de denúncias com o status passado
+                4 - GET de denúncias com o tipo passado
+                5 - POST de uma nova denúncia
+                6 - DELETE de uma denúncia
+                7 - PUT de uma denúncia
+        */
+
+        [HttpGet]
+        public ActionResult<List<Denuncia>> GetAll() {
+            return this._context.Denuncia.ToList();
+        }
+
+        [HttpGet("/cpf")]
+        public ActionResult<List<Denuncia>> GetDenunciasUsuario(String cpf) {
+            // ? 
+        }
     }
 }
