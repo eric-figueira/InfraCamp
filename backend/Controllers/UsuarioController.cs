84cce6fadc199782875e1474d6005df118de71fc
marcoscotuquense <113925560+Marcos-Godinho-Filho@users.noreply.github.com>
2023-04-12 23:07:17 -0300

    addded usuario controller
    - I can't use the api in my computer, I think it's a version compatibility issue

--- backend/Controllers/UsuarioController.cs
+++ backend/Controllers/UsuarioController.cs
@@ -16,8 +16,96 @@ namespace backend.Controllers
     public class UsuarioController : Controller
     {
         private InfraCampContext _context;
-        public UsuarioController(InfraCampContext ctx) {
+        public UsuarioController(InfraCampContext ctx)
+        {
             this._context = ctx;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Usuario>> GetAll()
+        {
+            return this._context.Usuario.ToList();
+        }
+
+        [HttpGet("{cpf}")]
+        public ActionResult<Usuario> GetUsuario(string cpf)
+        {
+            try
+            {
+                var resultado = this._context.Usuario.Find(cpf);
+                if (resultado == null)
+                    return NotFound();
+                return Ok(resultado);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCode.Status500InternalServerError, "Falha ao acesso no banco de dados.");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Usuario>> Post(Usuario usuario)
+        {
+            try
+            {
+                this._context.Usuario.Add(usuario);
+                if (await _context.SaveChangesAsync() == 1)
+                {
+                    return Created("api/usuarios/" + usuario.Cpf, usuario);
+                }
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCode.Status500InternalServerError, "Falha ao acesso no banco de dados.");
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete("{cpf}")]
+        public async Task<ActionResult<Usuario>> Delete(string cpf)
+        {
+            try
+            {
+                var resultado = await this._context.Usuario.FindAsync(cpf);
+                if (resultado == null)
+                    return NotFound();
+                this._context.Usuario.Remove(cpf);
+                await this._context.SaveChangesAsync();
+                return NoContent();
+            }
+            catch (Exception erro)
+            {
+                return this.StatusCode(StatusCode.Status500InternalServerError, "Falha ao acesso ao banco de dados.");
+            }
+        }
+
+        [HttpPut("{cpf}")]
+        public async Task<ActionResult<Usuario>> Put(string cpf, Usuario usuario)
+        {
+            try
+            {
+                var resultado = await this._context.Usuario.FindAsync(cpf);
+                if (resultado == null)
+                    return NotFound();
+                    
+                resultado.Cpf = usuario.Cpf;
+                resultado.Nome = usuario.Nome;
+                resultado.DataNascimento = usuario.DataNascimento;
+                resultado.Endereco = usuario.Endereco;
+                resultado.Email = usuario.Email;
+                resultado.Telefone = usuario.Telefone;
+                resultado.Senha = usuario.Senha;
+                resultado.UrlImagem = usuario.UrlImagem;
+                resultado.IsFunc = usuario.IsFunc;
+
+                await this._context.SaveChangesAsync();
+                return Created("api/alunos/" + usuario.Cpf, usuario);
+            }
+            catch (Exception erro)
+            {
+                return this.StatusCode(StatusCode.Status500InternalServerError, 
+                "Falha no acesso aos dados.");
+            }
         }
     }
 }
