0e6d5bb6f6ddfbcb552e80ef1fb4b7f19abf95d5
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2023-04-18 09:02:30 -0300

    AuthController

--- backend/Controllers/AuthController.cs
+++ backend/Controllers/AuthController.cs
@@ -14,6 +14,19 @@ namespace backend.Controllers
     [Route("api/auth")]
     public class AuthController : ControllerBase
     {
-         var tokenHandler = new JwtSecurityTokenHandler();
+        [HttpGet("/token/{email}/{password}")]
+        public String GetAuthenticationToken (string email, string password) 
+        {
+            try {
+                // Verificar se dados existem na tabela
+                // Gerar token
+                // token aleatorio apenas para testes
+                return "0d45cecd-f588-4007-a411-4298f6f4d5cc";
+            }
+            catch {
+                // Mudar o codigo
+                return this.StatusCode(StatusCodes.Status400InternalServerError, "Dados inválidos!");
+            }
+        }
     }
 }
