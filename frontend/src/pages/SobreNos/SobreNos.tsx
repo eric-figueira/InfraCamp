0b0aa3343f88392bed5cbf3cd4650787743c2599
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2023-05-03 11:04:43 -0300

    Carousel is working! Finished About Us page

--- frontend/src/pages/SobreNos/SobreNos.tsx
+++ frontend/src/pages/SobreNos/SobreNos.tsx
@@ -3,6 +3,7 @@ import React, { useState, useContext } from 'react';
 import "./SobreNos.css"
 
 import { imagens } from './imagens';
+import { CaretCircleLeft, CaretCircleRight } from 'phosphor-react'
 
 import Navbar from "../../components/Navbar/Navbar"
 
@@ -15,8 +16,12 @@ const SobreNos: React.FC = () => {
       <Navbar />
       <div className="carousel">
         <div className="carouselInner" style={{ backgroundImage: `url(${imagens[imgIndex].image})` }}>
-          <div className="left" onClick={() => imgIndex > 0 ? setImgIndex(imgIndex - 1) : setImgIndex(imagens.length - 1)}>a</div>
-          <div className="right" onClick={() => imgIndex < imagens.length - 1 ? setImgIndex(imgIndex + 1) : setImgIndex(0)}>a</div>
+          <div className="carousel-left-arrow" onClick={() => imgIndex > 0 ? setImgIndex(imgIndex - 1) : setImgIndex(imagens.length - 1)}>
+            <CaretCircleLeft color='#FFF' size={32}/>
+          </div>
+          <div className="carousel-right-arrow" onClick={() => imgIndex < imagens.length - 1 ? setImgIndex(imgIndex + 1) : setImgIndex(0)}>
+            <CaretCircleRight color='#FFF' size={32}/>
+          </div>
         </div>
       </div>
     </div>
