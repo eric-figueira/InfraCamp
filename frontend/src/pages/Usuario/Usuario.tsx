f0034f792ea4b98c955f2e060b0612ed6cc68363
URICH <107805174+BennethUrich@users.noreply.github.com>
2023-05-03 01:40:20 -0300

    Commit para corrigir coisas bobas
    oi gente bom dia!

--- frontend/src/pages/Usuario/Usuario.tsx
+++ frontend/src/pages/Usuario/Usuario.tsx
@@ -1,2 +1,17 @@
+<<<<<<< HEAD
+import React, { Fragment } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Usuario from "../../containers/Usuario/Usuario";
+
+import './Posts.css';
+
+const Usuario : React.FC = () => {
+    return (
+        <Fragment>
+            <Navbar/>
+            <Usuario/>
+        </Fragment>
+    )
+}
 
 export default Usuario;
