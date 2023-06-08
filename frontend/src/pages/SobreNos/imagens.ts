2308f51e54ca446832ac142b50d995a5d86e1340
URICH <107805174+BennethUrich@users.noreply.github.com>
2023-06-08 17:36:51 -0300

    Add carousel images
    oi marcos bom dia

--- frontend/src/pages/SobreNos/imagens.ts
+++ frontend/src/pages/SobreNos/imagens.ts
@@ -1,21 +1,42 @@
 import Logar from "../../assets/imgs/entrar.png";
 import Cadastrar from "../../assets/imgs/cadastrar.png";
 import Recuperar from "../../assets/imgs/recuperar.png";
+import crimg1 from "../../assets/imgs//CarrosselPhotos/CarrosselImage1.png"; 
+import crimg2 from "../../assets/imgs//CarrosselPhotos/CarrosselImage2.png"; 
+import crimg3 from "../../assets/imgs//CarrosselPhotos/CarrosselImage3.png"; 
+import crimg4 from "../../assets/imgs//CarrosselPhotos/CarrosselImage4.png"; 
+import crimg5 from "../../assets/imgs//CarrosselPhotos/CarrosselImage5.png"; 
+import crimg6 from "../../assets/imgs//CarrosselPhotos/CarrosselImage6.png"; 
 
 export const imagens = [
   {
-    label: 'Logar',
+    label: 'Imagem1',
     alt: 'Texto Alternativo 1',
-    image: Logar
+    image: crimg1
   },
   {
-    label: 'Cadastrar',
+    label: 'Imagem2',
     alt: 'Texto Alternativo 2',
-    image: Cadastrar
+    image: crimg2
   },
   {
-    label: 'Recuperar Senha',
+    label: 'Imagem3',
     alt: 'Texto Alternativo 3',
-    image: Recuperar
+    image: crimg3
+  },
+  {
+    label: 'Imagem4',
+    alt: 'Texto Alternativo 4',
+    image: crimg4
+  },
+  {
+    label: 'Imagem5',
+    alt: 'Texto Alternativo 5',
+    image: crimg5
+  },
+  {
+    label: 'Imagem6',
+    alt: 'Texto Alternativo 6',
+    image: crimg6
   }
 ]
